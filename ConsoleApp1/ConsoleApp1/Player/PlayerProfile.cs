fd5948d571f8f7c74baaf57afaa787c6f91652d0
Pew <memeris2@gmail.com>
2026-09-22 22:53:35 +0300

    Начальный коммит
    Здесь только папки с названиями и немного профиля игрока.

---
+++ ConsoleApp1/ConsoleApp1/Player/PlayerProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambling.Player;
+
+public class PlayerProfile
+{
+    public string Name { get; private set; }
+
+    public int Bank { get; private set; }
+
+    public PlayerProfile(string name)
+    {
+        Name = name;
+        Bank = 1000;
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿using Gambling.Player;
+
+PlayerProfile player = new PlayerProfile("Victor");
+
+Console.WriteLine(player.Name);
+Console.WriteLine(player.Bank);
